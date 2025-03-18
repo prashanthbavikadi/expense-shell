95e08b35ca3d70cadb61784ff79c1fc446ee3732
bavikadi <Prashanthbavikadiias@.com>
2025-03-19 03:42:39 +0530

    some-message

--- frontend-shell.sh
+++ frontend-shell.sh
@@ -46,3 +46,9 @@ cd /usr/share/nginx/html &>>$LOGFILE
 unzip /tmp/frontend.zip &>>$LOGFILE
 VALIDATE $? "Extracting frontend code"
 
+#check your repo and path
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+VALIDATE $? "Copied expense conf"
+
+systemctl restart nginx &>>$LOGFILE
+VALIDATE $? "Restarting nginx"
